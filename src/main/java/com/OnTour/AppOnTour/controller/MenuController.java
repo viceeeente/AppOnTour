67c1e0f1a3f8e0b4ac04985565d23ee1cbed166a
vice. <vice.alvarezm@duocuc.cl>
2025-06-12 21:16:10 -0400

    Modificacion de RequestMapping de MenuController y se agrego el nombre de los datos que se deberian mostrar en la opcion de Consultar Depositos

--- src/main/java/com/OnTour/AppOnTour/controller/MenuController.java
+++ src/main/java/com/OnTour/AppOnTour/controller/MenuController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/home")
 public class MenuController {
 
     @Autowired
